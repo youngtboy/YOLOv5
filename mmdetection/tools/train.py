88a4e83e9522f9ffa43d33b56466b1d1ccd4f2b7
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-03 15:42:08 +0800

    auto commit

--- mmdetection/tools/train.py
+++ mmdetection/tools/train.py
@@ -85,8 +85,14 @@ def parse_args():
 
 
 def main():
+    img_info = os.listdir("/home/data/323")
+    img_info.sort()
+    train_txt_path = "../train.txt"
+    with open(train_txt_path,"w") as fn:   
+        for i in range(len(img_info)//2):
+            fn.write(img_info[2*i].split(".")[0]+"\n")
+            
     args = parse_args()
-
     cfg = Config.fromfile(args.config)
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
