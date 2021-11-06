3d144e7997baab32827051a5add6896d027b9b03
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-06 22:39:02 +0800

    auto commit

--- mmdetection/configs/_base_/models/cascade_rcnn_r50_fpn.py
+++ mmdetection/configs/_base_/models/cascade_rcnn_r50_fpn.py
@@ -10,7 +10,7 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        init_cfg=None),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
