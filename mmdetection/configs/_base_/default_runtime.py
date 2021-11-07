e629a9d6f576183eeb2892a194808c09d64196e3
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-07 10:26:57 +0800

    auto commit

--- mmdetection/configs/_base_/default_runtime.py
+++ mmdetection/configs/_base_/default_runtime.py
@@ -1,7 +1,7 @@
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=50,
+    interval=500,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook')
