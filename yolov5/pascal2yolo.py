31d243559707b87fbc08ce2f47c48528fe181a13
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-11 18:34:04 +0800

    auto commit

---
+++ yolov5/pascal2yolo.py
@@ -0,0 +1,96 @@
+import os
+from lxml import etree
+
+voc_images_path = "../SmokeDataset/VOC2007/JPEGImages"
+voc_xml_path = "../SmokeDataset/VOC2007/Annotations"
+class_dict = {"smoke": 0}
+
+
+def parse_xml_to_dict(xml):
+    """
+    将xml文件解析成字典形式，参考tensorflow的recursive_parse_xml_to_dict
+    Args：
+        xml: xml tree obtained by parsing XML file contents using lxml.etree
+
+    Returns:
+        Python dictionary holding XML contents.
+    """
+
+    if len(xml) == 0:  # 遍历到底层，直接返回tag对应的信息
+        return {xml.tag: xml.text}
+
+    result = {}
+    for child in xml:
+        child_result = parse_xml_to_dict(child)  # 递归遍历标签信息
+        if child.tag != 'object':
+            result[child.tag] = child_result[child.tag]
+        else:
+            if child.tag not in result:  # 因为object可能有多个，所以需要放入列表里
+                result[child.tag] = []
+            result[child.tag].append(child_result[child.tag])
+    return {xml.tag: result}
+
+
+def convert_info(file_names, save_root):
+
+    save_txt_path = os.path.join(save_root, "labels")
+    f = open(file_names, "r")
+    for line in f:
+        name = line.rstrip()
+
+        # 检查下图像文件是否存在
+        img_path = os.path.join(voc_images_path, name + ".jpg")
+        assert os.path.exists(img_path), "file:{} not exist...".format(img_path)
+
+        # 检查xml文件是否存在
+        xml_path = os.path.join(voc_xml_path, name + ".xml")
+        assert os.path.exists(xml_path), "file:{} not exist...".format(xml_path)
+
+        # read xml
+        with open(xml_path) as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = parse_xml_to_dict(xml)["annotation"]
+        img_height = int(data["size"]["height"])
+        img_width = int(data["size"]["width"])
+
+        # write object info into txt
+        assert "object" in data.keys(), "file: '{}' lack of object key.".format(xml_path)
+        if len(data["object"]) == 0:
+            # 如果xml文件中没有目标就直接忽略该样本
+            print("Warning: in '{}' xml, there are no objects.".format(xml_path))
+            continue
+
+        with open(os.path.join(save_txt_path, name + ".txt"), "w") as f:
+            for index, obj in enumerate(data["object"]):
+                # 获取每个object的box信息
+                xmin = float(obj["bndbox"]["xmin"])
+                xmax = float(obj["bndbox"]["xmax"])
+                ymin = float(obj["bndbox"]["ymin"])
+                ymax = float(obj["bndbox"]["ymax"])
+                class_name = obj["name"]
+                class_index = class_dict[class_name]  # 目标id从0开始
+
+                # 进一步检查数据，有的标注信息中可能有w或h为0的情况，这样的数据会导致计算回归loss为nan
+                if xmax <= xmin or ymax <= ymin:
+                    print("Warning: in '{}' xml, there are some bbox w/h <=0".format(xml_path))
+                    continue
+
+                # 将box信息转换到yolo格式
+                xcenter = xmin + (xmax - xmin) / 2
+                ycenter = ymin + (ymax - ymin) / 2
+                w = xmax - xmin
+                h = ymax - ymin
+
+                # 绝对坐标转相对坐标，保存6位小数
+                xcenter = round(xcenter / img_width, 6)
+                ycenter = round(ycenter / img_height, 6)
+                w = round(w / img_width, 6)
+                h = round(h / img_height, 6)
+
+                info = [str(i) for i in [class_index, xcenter, ycenter, w, h]]
+
+                if index == 0:
+                    f.write(" ".join(info))
+                else:
+                    f.write("\n" + " ".join(info))
