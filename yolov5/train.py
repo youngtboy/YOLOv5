43bf591208b84aa4ad6985c46c7a3d77fa2f9e8f
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-11 17:06:02 +0800

    auto commit

--- yolov5/train.py
+++ yolov5/train.py
@@ -622,11 +622,13 @@ def run(**kwargs):
 
 
 if __name__ == "__main__":
+    if os.exits("/home/data/323/images.txt):
+        os.remove("/home/data/323/images.txt")
     img_info = os.listdir("/home/data/323")
     img_info.sort()
     train_txt_path = "/home/data/323/images.txt"
     with open(train_txt_path,"w") as fn:   
         for i in range(len(img_info)//2):
-            fn.write(os.path.join("/home/data/123",i)+"\n")
+            fn.write(os.path.join("/home/data/123/",img_info[i]+"\n")
     opt = parse_opt()
     main(opt)
