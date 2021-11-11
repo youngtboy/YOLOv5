6e0ac7fc00572b32195cb5ede7546e49a4402044
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-11 16:02:03 +0800

    auto commit

---
+++ yolov5/prepare_data.py
@@ -0,0 +1,8 @@
+import os
+
+img_info = os.listdir("/home/data/323")
+img_info.sort()
+train_txt_path = "./train.txt"
+with open(train_txt_path,"w") as fn:   
+    for i in range(len(img_info)//2):
+        fn.write(os.path.join("/home/data/123",img_info[2*i].split(".")[0]+"\n"))
