7484d37cb0076f9e1cf149c74e960e1d51952165
cvmart-dev-24222-tencent <cvmart-dev-24222-tencent@cvmart.net>
2021-11-11 19:42:03 +0800

    auto commit

--- yolov5/prepare_data.py
+++ yolov5/prepare_data.py
@@ -1,7 +1,9 @@
 import os
 import shutil
 
-image_path = ""
+image_path = "/home/data/323/SmokeDatset/images"
 img_list = os.listdir(image_path)
 for i in img_list:
-    shutil
+    shutil.move(os.path.join(image_path,i),"/home/data/323/")
+
+ann_list
